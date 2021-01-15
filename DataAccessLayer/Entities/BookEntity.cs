e11349c5e56ff58469bc27d8697da266c4cbc825
oguzkose <oguzkose2019@outlook.com>
2021-01-15 16:57:50 +0300

    Dll layer created and EF package added

---
+++ DataAccessLayer/Entities/BookEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.Entities
+{
+    public class BookEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+    }
+}

dea453ba2b1c66966de10922a51d057ba284470d
oguzkose <oguzkose2019@outlook.com>
2021-01-16 03:59:40 +0300

    added BookController and Index Action Method

---
+++ BookStore/Controllers/BookController.cs
@@ -0,0 +1,35 @@
+﻿using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookService _service;
+        public BookController(BookService service)
+        {
+            _service = service;
+        }
+        public IActionResult Index()
+        {
+            var bookEntities = _service.GetAllBooks();
+            var model = new List<BookViewModel>();
+            foreach (var entity in bookEntities)
+            {
+                model.Add(new BookViewModel
+                {
+                    Id = entity.Id,
+                    Name = entity.Name,
+                    Author = entity.Author,
+                    Publisher = entity.Publisher
+                });
+            }
+            return View(model);
+        }
+    }
+}
